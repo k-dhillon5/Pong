4a34f1d2ea0ee9718413b4acb5e096fd41fbed57
k-dhillon5 <83429474+k-dhillon5@users.noreply.github.com>
2022-01-20 13:04:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 # setup
 pygame.init()
 
@@ -17,15 +18,21 @@ pygame.display.set_caption('Pong')
 
 
 def move_ball():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, playerScore, oppScore
 
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1
+
+    if ball.left <= 0:
+        playerScore += 1
+        ball_restart()
+
+    if ball.right >= screen_width:
+        ball_restart()
+        oppScore += 1
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
@@ -51,6 +58,14 @@ def auto_opp():
         opponent.bottom = screen_height
 
 
+def ball_restart():
+    global ball_speed_x, ball_speed_y
+
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_y *= random.choice((1, -1))
+    ball_speed_x *= random.choice((1, -1))
+
+
 # platforms
 ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
 player = pygame.Rect(screen_width - 20, screen_width/2 - 30, 10, 140)
@@ -60,6 +75,10 @@ ball_speed_x = 6
 ball_speed_y = 6
 player_speed = 0
 opponent_speed = 7
+
+playerScore = 0
+oppScore = 0
+scoretext = pygame.font.Font("freesansbold.ttf", 30)
 
 while True:
     for event in pygame.event.get():
@@ -86,6 +105,12 @@ while True:
     pygame.draw.ellipse(screen, Grey, ball)
     pygame.draw.aaline(screen, Black, (screen_width/2, 0),
                        (screen_width/2, screen_height))
+
+    playerText = scoretext.render(f"{playerScore}", False, Grey)
+    screen.blit(playerText, (280, 30))
+
+    oppText = scoretext.render(f"{oppScore}", False, Grey)
+    screen.blit(oppText, (200, 30))
 
     pygame.display.flip()
     clock.tick(60)
